2f6649195ff67d12df7e686ba67bcdb117b58678
xMaskman <maskmandiscord@gmail.com>
2025-07-24 08:34:56 +0600

    refactoring some code

--- background.js
+++ background.js
@@ -20,6 +20,106 @@ async function tabAudioCollector(){
 tabAudioCollector();
 
 
+async function switchingtonextaudibletab(){
+
+  tabAudioCollector();
+  audibletabslength = alltabindex.length;
+
+  if (command === "tabswitchingtoaudioplayingtab"){
+
+    console.log(alltabindex)
+    console.log('Switching to tab ID:', alltabindex[count]);
+    await browser.tabs.update(alltabindex[count],{active: true})
+  
+
+    count++;
+    count = count % audibletabslength;
+    console.log(count)
+
+    if (Number.isNaN(count)) {
+      count = 0;
+    }
+    
+
+
+}
+
+
+
+
+
+}
+
+async function tabsortercommand(){
+
+  console.log("The command is running")
+
+  if (command === "Tab sorter"){
+
+    const tabcollector = {}
+
+    const tabs = await browser.tabs.query({});
+
+    tabs.forEach(Tab =>{
+
+    console.log(Tab.title)
+    let tabid = Tab.id
+    let taburl = Tab.url
+
+
+    //check if the object is already in dictionary
+    // if the dict has the object then it appends to the array
+    if(tabcollector.hasOwnProperty(taburl)){
+
+    tabcollector[taburl].tabid1.push(tabid)
+
+    //If the object isn't in the dict it creates an array.
+
+    }else{
+      
+    tabcollector[taburl] = {
+
+    tabid1: [tabid]
+    }}})
+
+    // Collects all of the parent dict and sorts it inside an array
+    let domainlist32 = Object.keys(tabcollector).sort()
+
+
+    // Checks the length all of the array
+    domainlength = domainlist32.length;
+
+    
+
+    let alltabid = []
+
+    // iterate through url array
+    for(let i = 0;i<domainlength;i++){
+      let temp = domainlist32[i];
+    
+      let tempsize = tabcollector[temp].tabid1
+      let tempsizelength = tempsize.length
+      
+      for(let i = 0;i<tempsizelength;i++){
+        let temp2 = tempsize[i]
+        alltabid.push(temp2)
+        
+
+      }
+
+    alltabidlength = alltabid.length;
+    for(let i = 0;i<alltabidlength;i++){
+
+  await browser.tabs.move(alltabid[i],{index: i});
+  
+}}}}
+
+
+
+
+
+
+
 
 browser.commands.onCommand.addListener(async (command) => {
 
@@ -51,14 +151,7 @@ browser.commands.onCommand.addListener(async (command) => {
     tabcollector[taburl] = {
 
     tabid1: [tabid]
-
-    }} 
-
-
-  
-      
-      
-    })
+    }}})
 
     // Collects all of the parent dict and sorts it inside an array
     let domainlist32 = Object.keys(tabcollector).sort()
